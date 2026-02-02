ed6482d7cdc4594abb8a95dc5c96df0fd75ed9ea
tuanemtramtinh <anh.nguyen2252038@hcmut.edu.vn>
2026-02-02 17:53:10 +0700

    SCRUM-18: Adding extend and minize sidebar

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,29 +1,50 @@
+import { Menu, PanelLeftOpen, PanelRightOpen } from "lucide-react";
+import { useState } from "react";
 import { Outlet } from "react-router";
 
 export default function AdminLayout() {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+
   return (
     <div className="drawer lg:drawer-open">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <input
+        id="my-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+        defaultChecked={isOpen}
+        onChange={(e) => setIsOpen(e.target.checked)}
+      />
       <div className="drawer-content">
         <div className="p-4">
           <Outlet />
         </div>
       </div>
-      <div className="drawer-side">
+      <div className="drawer-side is-drawer-close:overflow-visible">
         <label
-          htmlFor="my-drawer-3"
+          htmlFor="my-drawer"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-admin-sidebar min-h-full w-[15vw] p-4 text-white">
-          {/* Sidebar content here */}
-          <li>
-            <a className="">Sidebar Item 1</a>
-          </li>
-          <li>
-            <a className="">Sidebar Item 2</a>
-          </li>
-        </ul>
+        <div className="bg-admin-sidebar is-drawer-close:w-[5vw] is-drawer-open:w-[15vw] flex min-h-full flex-col items-start p-4 text-white">
+          <div className="is-drawer-open:justify-end is-drawer-close:justify-center flex w-full">
+            <label
+              htmlFor="my-drawer"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              {isOpen ? <PanelRightOpen /> : <PanelLeftOpen />}
+            </label>
+          </div>
+          <ul className="menu w-full">
+            {/* Sidebar content here */}
+            <li>
+              <a className="">Sidebar Item 1</a>
+            </li>
+            <li>
+              <a className="">Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
